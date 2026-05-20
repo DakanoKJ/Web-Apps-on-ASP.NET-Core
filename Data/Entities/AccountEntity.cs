61431126ae0bc4276fa9b7c9bb0c3505e5f987c6
Alexandr Shamraev <alexandr.shamraev.github@gmail.com>
2026-05-20 19:58:42 +0300

    Сущность студента разделена на две - Аккаунт и профиль студента - с целью разделения ролей в будущем

--- Data/Entities/AccountEntity.cs
+++ Data/Entities/AccountEntity.cs
@@ -1,14 +1,15 @@
-﻿using PersonalAccount.Models.Students;
+﻿using PersonalAccount.Types;
 
 namespace PersonalAccount.Data.Entities;
 
-public class StudentEntity
+public class AccountEntity
 {
     public int Id { get; set; }
-    public string FullName { get; set; } = string.Empty;
-    public string GroupName { get; set; } = string.Empty;
+    
     public string Email { get; set; } = string.Empty;
-    public string? PhotoUrl { get; set; }
     public string PasswordHash { get; set; } = string.Empty;
+    public AccountRole Role { get; set; } = AccountRole.Student;
+
+    public StudentProfileEntity? StudentProfile { get; set; }
     public List<ConfirmationTokenEntity> ConfirmationTokens { get; set; } = [];
 }
