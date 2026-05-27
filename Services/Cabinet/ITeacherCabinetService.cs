f204a188f7b6ea8cb837c74c83f03e7843b1ff45
6ECCMEPTHIoIIJ <shamraev.alexandr.github@gmail.com>
2026-05-27 20:50:47 +0300

    Инъекция зависимостей

---
+++ Services/Cabinet/ITeacherCabinetService.cs
@@ -0,0 +1,9 @@
+﻿using PersonalAccount.Models;
+
+namespace PersonalAccount.Services.Cabinet;
+
+public interface ITeacherCabinetService
+{
+    Task<Dictionary<int, List<GroupModel>>> GetAllTeacherGroupsGroupedBySubjectsAsync();
+    Task<Dictionary<int, List<SubjectModel>>> GetAllTeacherSubjectsGroupedByGroupsAsync();
+}
